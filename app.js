e54f586cea061c11a6551e4aec3694a413e7b3f3
Joe-Mwangi <joem99271@gmail.com>
2023-02-25 07:23:57 +0300

    complete

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ require('dotenv').config()
 const path = require('path')
 const express = require('express')
 const helmet = require('helmet')
-const infoRouter = require('./routers/info/info.router')
-const authRouter = require('./routers/auth/auth.router')
 const passport = require('passport')
 const {Strategy} = require('passport-google-oauth20')
 const cookieSession = require('cookie-session')
@@ -24,13 +22,24 @@ const authOptions = {
 }
 
 function verifyCallback(accessToken, refreshToken, profile, done) {
-    console.log('Google profile', profile)
+    // console.log('Google profile', profile)
     done(null, profile)
 }
 
 passport.use(new Strategy(authOptions, verifyCallback))
 
-//use helmet before all middlewares
+//save the session to the cookie
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+//Read the session from the cookie
+passport.deserializeUser((id, done) => {
+    done(null, id)
+})
+
+
+// use helmet before all middlewares
 app.use(helmet())
 
 app.use(cookieSession({
@@ -38,10 +47,12 @@ app.use(cookieSession({
     maxAge: 24 * 60 * 60 * 1000,
     keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2]
 }))
+
 app.use(passport.initialize())
+app.use(passport.session())
 
 function checkLoggedIn(req, res, next) {
-    const isLoggedIn = true
+    const isLoggedIn = req.isAuthenticated() && req.user
     if(!isLoggedIn) {
         return res.status(401).json({
             error: 'You must log in'
@@ -50,7 +61,6 @@ function checkLoggedIn(req, res, next) {
     next()
 }
 
-// app.use(authRouter)
 
 app.get('/auth/google', 
     passport.authenticate('google', {
@@ -62,15 +72,18 @@ app.get('/auth/google/callback',
     passport.authenticate('google', {
         failureRedirect: '/failure',
         successRedirect: '/',
-        session: false
     }), (req, res) => {
         console.log('Google called us back')
 })
 
-// app.use(infoRouter)
 
 app.get( '/',(req, res) => {
     return res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+app.get('/auth/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/')
 })
 
 app.get( '/secret', checkLoggedIn,(req, res) => {
